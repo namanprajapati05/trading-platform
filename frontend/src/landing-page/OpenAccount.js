1168f5a3365c81dfc583a1651f4063e22e2899a5
namanprajapati05 <namanprajapati819@gmail.com>
2026-08-23 15:22:29 +0530

    Fix frontend folder tracking and add source files

---
+++ frontend/src/landing-page/OpenAccount.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OpenAccount() {
+    return ( <>
+    opneaccount
+    </> );
+}
+
+export default OpenAccount;
---
+++ frontend/src/landing-page/home/Stats.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Stats() {
+  return (
+    <>Stats</>
+  );
+}
+
+export default Stats;
