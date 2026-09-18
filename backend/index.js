158b339accae1cdcd06f682e00c0ca0d0a15096c
namanprajapati05 <101320748+namanprajapati05@users.noreply.github.com>
2026-09-18 11:36:37 +0530

    Add production URL to CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware 
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173" , "https://trading-dashboard-omega-flax.vercel.app/"],
     credentials: true,
   }));
   
