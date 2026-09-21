912c42aacf65339fa350b464e3cfa19c9d15a617
namanprajapati05 <101320748+namanprajapati05@users.noreply.github.com>
2026-09-21 14:22:36 +0530

    Add new CORS origin for trading dashboard

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const io = new Server(server, {
         origin: [
             "http://localhost:5173",
             "http://localhost:3000",
-            "https://trading-dashboard-7o92f56tn-namans-projects-b6811f1a.vercel.app"
+            "https://trading-dashboard-7o92f56tn-namans-projects-b6811f1a.vercel.app",
+            "https://trading-dashboard-omega-flax.vercel.app/"
         ],
         methods: ["GET", "POST"],
         credentials: true,
